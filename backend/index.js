d9f1e91ae785f311948e241a840b3917b3907d35
ShivangM <shivangmishra0824@gmail.com>
2022-01-14 00:12:58 +0530

    rev

--- backend/index.js
+++ backend/index.js
@@ -91,9 +91,9 @@ app.post("/search", async (req, res) => {
 })
 
 // AFTER defining routes: Anything that doesn't match what's above, send back index.html; (the beginning slash ('/') in the string is important!)
-// app.get('*', (req, res) => {
-//   res.sendFile(path.join(__dirname + '/../frontend/build/index.html'))
-// })
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname + '/../frontend/build/index.html'))
+})
 //Server Hosting
 const port = process.env.PORT
 app.listen(port, () => {
